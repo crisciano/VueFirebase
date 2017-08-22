b79ae679e5afcb8b5befe28b71ce6d16a42c66d4
Crisciano S. Botelho <cris@apeppers.com.br>
2017-08-22 13:56:21 -0300

    firebase working

--- src/main.js
+++ src/main.js
@@ -1,5 +1,16 @@
 import Vue from 'vue'
 import App from './App.vue'
+import firebase from 'firebase'
+
+// Setup Firebase
+var config = {
+  apiKey: "AIzaSyAi_yuJciPXLFr_PYPeU3eTvtXf8jbJ8zw",
+  authDomain: "vue-demo-537e6.firebaseapp.com",
+  databaseURL: "https://vue-demo-537e6.firebaseio.com"
+}
+firebase.initializeApp(config)
+
+var usersRef = firebase.database().ref('users')
 
 new Vue({
   el: '#app',
